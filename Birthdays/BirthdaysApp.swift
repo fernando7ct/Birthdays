9df412513d86436951ade72bc046f72fcfa31c19
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-11-08 17:25:27 -0600

    Done.

--- Birthdays/BirthdaysApp.swift
+++ Birthdays/BirthdaysApp.swift
@@ -1,17 +1,12 @@
-//
-//  BirthdaysApp.swift
-//  Birthdays
-//
-//  Created by Fernando Caudillo Tafoya on 11/8/25.
-//
-
 import SwiftUI
+import SwiftData
 
 @main
 struct BirthdaysApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Friend.self)
         }
     }
 }
